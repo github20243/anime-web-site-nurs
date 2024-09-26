61aae0657a5b4109b9ecbaa1bbcbd9c20692f62c
nurs_996 <98372@gamil.com>
2024-09-26 18:16:13 +0600

    saveing

--- src/store/request/requestAnime.ts
+++ src/store/request/requestAnime.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { Anime, AnimeInfo, AnimeEpisode } from "../../types/types";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
+const ANIME_CART_URL = import.meta.env.VITE_APP_URL;
 const ANIME_INFO_URL = import.meta.env.VITE_APP_ANIME_INFO_URL;
 const EPISODES_VIDEO_URL = import.meta.env.VITE_APP_EPISODES_VIDEO_URL;
 
@@ -11,15 +12,12 @@ export const getAnimes = createAsyncThunk<
 	{ rejectValue: string }
 >("animes/getAnimes", async (_, { rejectWithValue }) => {
 	try {
-		const response = await fetch(
-			"https://caa3b07d2a4f13be.mokky.dev/anime-videos",
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			}
-		);
+		const response = await fetch(ANIME_CART_URL, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
 		if (!response.ok) {
 			throw new Error(`Ошибка сети: ${response.status} ${response.statusText}`);
